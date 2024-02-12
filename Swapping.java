d9a117e6cba2450e8078f526a744c5fffc9ee480
Jay Millena <jay.millena07@gmail.com>
2024-02-12 17:44:52 +0800

    Initial commit

---
+++ Swapping.java
@@ -0,0 +1,11 @@
+public class Swapping 
+{
+  public static void main(String[] args)
+  {
+    String first_student = "John";
+    String second_student = "Mark";
+    String third_student = "Rocky";
+
+    System.out.println("The original students are "+ first_student +", " + second_student +", and "+ third_student);
+  }
+}
